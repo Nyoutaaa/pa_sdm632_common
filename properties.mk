e59014f2436458385ef93cb4642fb8fd42509338
Andrew Hexen <SyberHexen@gmail.com>
2020-09-05 19:23:05 -0700

    sdm632-common:wip: zram writeback..etc..
    Change-Id: Ib312d1ebd5f5214e9abfc388dab1a85dc9c28edf

--- properties.mk
+++ properties.mk
@@ -276,4 +276,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0
+        wifi.interface=wlan0
+	wifi.interface=wlan0
+
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
+
